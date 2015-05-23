934690b49cc7219c58e4d847ee121cdff0c08bc3
Tory <none@none.com>
2015-05-23 13:17:40 -0400

    set transformations

--- one/src/main/scala/commonTrans.scala
+++ one/src/main/scala/commonTrans.scala
@@ -9,13 +9,15 @@ object commonTrans {
 
     // create RDD from range (1 to 55)
     // could have also List(1,2,3,...55)
-    val i = sc.parallelize(List(List(1,2,3), List(4,5,6)))
-    val nums = i.flatMap(x => x)
-    nums.foreach(x => {
-      println("--")
-      println(x)
-    })
-    println("======")
-    println(nums.first())
+    val i = sc.parallelize(List("cheese", "rooster", "duck", "rooster"))
+    val ii = sc.parallelize(List("bird", "cheese", "rabbit"))
+
+    println("i", i.collect.mkString(", "))
+    println("ii", ii.collect.mkString(", "))
+    println("distinct i", i.distinct().collect.mkString(", "))
+    println("union", i.union(ii).collect.mkString(", "))
+    println("intersection", i.intersection(ii).collect.mkString(", "))
+    println("i - ii", i.subtract(ii).collect.mkString(", "))
+    println("all possible pairs of i and ii", i.cartesian(ii).collect.mkString(", "))
   }
 }
