6b64ce492f76505baf7e2d585634edabca487d52
Tory <none@none.com>
2015-05-23 11:19:14 -0400

    take action

--- one/src/main/scala/cheese.scala
+++ one/src/main/scala/cheese.scala
@@ -8,39 +8,15 @@ object cheese {
     val sc = new SparkContext(conf)
     val input = sc.textFile("/home/freyja/cool.txt")
 
-    val hiRDD = input.filter(line => line.contains("hi"))
-
-    hiRDD.foreach(println)
-
-    println("--------")
-
-    val byeRDD = input.filter(line => line.contains("bye"))
-    byeRDD.foreach(println)
-
-    println("--------")
-
-    val greetings = hiRDD.union(byeRDD)
-
-    greetings.foreach(println)
-
-    println("Better RDD")
-
-    val betterRDD = input.filter(
-      line => List("Hi", "bye").exists(line.contains)
-    )
-
-    val betterRDDVerbose = input.filter(
+    val hibyeRDD = input.filter(
       (line: String) => {
-        // runs each itme in the lists until the function within exists returns
-        // true.
-        // the function inside exists checks to see if the line contains n.
-        // n which would be each list item one at a time until true is returned
         List("Hi", "bye").exists((n) => line.contains(n))
       }
     )
 
-    betterRDD.foreach(println)
-    print("=====")
-    betterRDDVerbose.foreach(println)
+    println("------")
+    hibyeRDD.foreach(println)
+    println("------")
+    hibyeRDD.take(2).foreach(println)
   }
 }
